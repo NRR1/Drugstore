377603f22aba55783efff525f74a655bdcc3aa41
NRR <pianzovmaxsim236@gmail.com>
2025-04-07 17:13:25 +0300

    Добавление модели

---
+++ Drugstore_Task/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drugstore_Task.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Check(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Contact");
+        }
+    }
+}
+
--- Drugstore_Task/Controllers/HomeController.cs
+++ Drugstore_Task/Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace Drugstore_Task.Controllers
         {
             return View();
         }
-
+        public IActionResult Contact() //  contacts/about
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
