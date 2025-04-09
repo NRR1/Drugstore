2bc4a1d8b5c0bdf535819b70d596226e2e3db119
NR <95234863+sraww@users.noreply.github.com>
2025-04-09 17:08:40 +0300

    Update HomeController.cs

--- Drugstore_Task/Controllers/HomeController.cs
+++ Drugstore_Task/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Drugstore_Task.Controllers
         {
             return View();
         }
-        public IActionResult Contacts() // contacts/about
+        public IActionResult About() // contacts/about
         {
             return View();
         }
