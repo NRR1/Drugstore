b54f0e9da0269405d3c9245958cf9a824ca83f44
NR <95234863+sraww@users.noreply.github.com>
2025-04-10 18:23:14 +0300

    Update Feedback.cs
    fix

--- Drugstore_Task/Models/Feedback.cs
+++ Drugstore_Task/Models/Feedback.cs
@@ -25,6 +25,7 @@ namespace Drugstore_Task.Models
         public string Message { get; set; }
         [Display(Name = " Введите ваш номер телефона")]
         [StringLenght(0,? ErrorMessage = "Поле не должно быть пустым")]
+        public int Number
         {
     }
 }
