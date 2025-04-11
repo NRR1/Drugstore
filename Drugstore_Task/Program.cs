f0b3eb41bb23462c54718ef32cbfb9eb96f742ab
NR <95234863+sraww@users.noreply.github.com>
2025-04-11 16:39:53 +0300

    Update Program.cs

--- Drugstore_Task/Program.cs
+++ Drugstore_Task/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+
 builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
    .AddNegotiate();
 
