56afa5f2251fd6dbff22676278bf1d3192393bef
NRR <pianzovmaxsim236@gmail.com>
2025-04-13 10:09:09 +0300

    Исправление ошибок в модели

--- Drugstore_Task/Models/Feedback.cs
+++ Drugstore_Task/Models/Feedback.cs
@@ -24,8 +24,8 @@ namespace Drugstore_Task.Models
         [StringLength(0, ErrorMessage ="Сообщение не может быть пустым!")]
         public string Message { get; set; }
         [Display(Name = " Введите ваш номер телефона")]
-        [StringLenght(0,? ErrorMessage = "Поле не должно быть пустым")]
+        [StringLength(0, ErrorMessage ="Поле не должно быть пустым")]
         public int Number { get; set; }
-        {
+        
     }
 }
