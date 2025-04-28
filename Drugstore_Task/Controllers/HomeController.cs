64580e298942241bc8c7098249872b434cb1153a
NRR <pianzovmaxsim236@gmail.com>
2025-04-28 17:26:49 +0300

    update

--- Drugstore_Task/Controllers/HomeController.cs
+++ Drugstore_Task/Controllers/HomeController.cs
@@ -31,5 +31,6 @@ namespace Drugstore_Task.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public 
     }
 }
