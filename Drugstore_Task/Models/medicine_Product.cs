f936967222e2d69092bda0308d0fa7f9f8b57449
NRR <pianzovmaxsim236@gmail.com>
2025-04-28 17:24:38 +0300

    fix model update

--- Drugstore_Task/Models/medicine_Product.cs
+++ Drugstore_Task/Models/medicine_Product.cs
@@ -4,16 +4,17 @@ namespace Drugstore_Task.Models
 {
     public class medicine_Product
     {
-        [Display(Name = " Назовите название лекарства.")]
+        [Display(Name = "Скажите название лекарства.")]
         [Required(ErrorMessage = "Такого лекарства нет.")]
         public string Name_Product { get; set; }
 
-        [Display(Name = "Производитель лекарства")]
+        [Display(Name = "Назовите производителя лекарства")]
         [Required(ErrorMessage = "Такого производителя нет")]
         public string manufacturer { get; set; }
 
         [Display(Name = " Год выпуска")]
         [Required(ErrorMessage = "Год выпуска не найден.")]
         public string Year { get; set; }
+
     }
 }
