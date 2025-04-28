750c156e57e404a817dac4f8d1d82d63384d77d8
NRR <pianzovmaxsim236@gmail.com>
2025-04-28 17:19:18 +0300

    add

--- Drugstore_Task/Models/medicine_Product.cs
+++ Drugstore_Task/Models/medicine_Product.cs
@@ -1,7 +1,11 @@
-﻿namespace Drugstore_Task.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Drugstore_Task.Models
 {
     public class medicine_Product
     {
-
+        [Display(Name = " Введите Имя")]
+        [Required(ErrorMessage = "Вам нужно ввести  ваше имя")]
+        public string Name { get; set; }
     }
 }
