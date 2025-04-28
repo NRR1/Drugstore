6caf9cd1fdaf30e2e8993dbbe8405c278ed7b30b
NRR <pianzovmaxsim236@gmail.com>
2025-04-28 17:29:09 +0300

    fix

--- Drugstore_Task/Controllers/HomeController.cs
+++ Drugstore_Task/Controllers/HomeController.cs
@@ -31,9 +31,9 @@ namespace Drugstore_Task.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public  IActionResult MedicineController()
-        {
-             
-        }
+        //public  IActionResult MedicineController()
+        //{
+           
+        //}
     }
 }
