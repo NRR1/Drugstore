d23083ac3be93e9858fb156b6b9868fdb452cc35
NRR <pianzovmaxsim236@gmail.com>
2025-05-02 09:47:28 +0300

    fix

--- Drugstore_Task/Controllers/MedicineController.cs
+++ Drugstore_Task/Controllers/MedicineController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Drugstore_Task.Controllers
 {
 
-      public class MedicineController : Controller
+    public class MedicineController : Controller
     {
         // GET: MedicineController
         public ActionResult Index()
