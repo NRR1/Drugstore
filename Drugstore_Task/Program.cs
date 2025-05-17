ac0db8a63c73649b2efe80e356e24961827b5a0d
AvdoshkaMMM <AvdoshkaMMM@DESKTOP-ER3FDUM>
2025-05-17 23:48:06 +0300

    Ыыыыыыыыыыы

--- Drugstore_Task/Program.cs
+++ Drugstore_Task/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddAuthorization(options =>
     // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
 });
-//builder.Services.AddRazorPages();
+//builder.Services.AddRazorPages(); ssssssss
 
 var app = builder.Build();
 
